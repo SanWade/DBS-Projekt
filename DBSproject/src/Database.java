05f48fb59cd0883bef7d25917f2144367f918d64
SanWade <big-sister2000@hotmail.de>
2014-06-23 22:57:07 +0200

    Features eingefügt
    alle 3 im Blatt geforderten Features sind eingefügt und laufen als
    Konsolenausgabe

--- DBSproject/src/Database.java
+++ DBSproject/src/Database.java
@@ -20,13 +20,15 @@
       
       Connection connection = null;
      
-      //* Constructor
+      //* Constructor, lädt DriverClass, öffnet Verbindung zur DB, ruft Features auf, schließt Verbindung
       
       public Database ()
       {
         loadJdbcDriver ();
         openConnection ();
         showTore ();
+        showGegenTore ();
+        showNiederlagen ();
         closeConnection ();
       }
      
@@ -68,7 +70,7 @@
         System.out.println ("driver loaded");
       }
      
-      //Verbindung öffnen
+      //Verbindung öffnen mittels URL,user,password
       private void openConnection () {
         try {
           connection = DriverManager.getConnection (getUrl (),user,password);
@@ -82,76 +84,41 @@
      
      
        
-     /* private void showTore() {
-        try {
-          Statement statement = connection.createStatement ();
      
-          ResultSet resultSet = statement.executeQuery (		  
-        		  "SELECT V.Name, sum(Tore_Heim) as Tore_Heim,sum(Tore_Gast) as Tore_Gast FROM Verein V,Spiel S WHERE (V.V_ID = S.Heim  OR V.V_ID = S.Gast) AND S.Datum IN (Select distinct Datum FROM Spiel Order by Datum desc FETCH FIRST 3 ROWS ONLY) GROUP BY V.Name"
-        		  );
-          ResultSetMetaData resultSetMetaData = resultSet.getMetaData ();
-     
-          System.out.println ("\n Feature 1: Tore der letzten 3 Spiele\n");
-     
-          String format = "%35s   %-6s   %6s \n";
-     
-          System.out.printf (format + "\n",
-                             resultSetMetaData.getColumnLabel (1),
-                             resultSetMetaData.getColumnLabel (2),
-                             resultSetMetaData.getColumnLabel (3)
-                             );
-
-          while (resultSet.next ())
-          {
-            System.out.printf (format,
-                               resultSet.getString (1),
-                               resultSet.getString (2),
-                               resultSet.getString (3)
-                               );
-          }
-     
-          resultSet.close ();
-          statement.close ();
-        }
-        catch (SQLException e){
-          e.printStackTrace ();
-          System.exit (1);
-        }
-      }
-     */
-      
       
     //Feature1: 'Tore der letzten 3 Spiele' 
       private void showTore() {
         try {
           Statement statement = connection.createStatement ();
      
-          String query1 = "Select neu.Name, sum(neu.Tore) "+
-        		  			"From ( "+
-        		  			"SELECT V.Name, sum(S.Tore_Heim) as Tore "+
-        		  			"FROM Verein V,Spiel S "+
-        		  			"WHERE (V.V_ID = S.Heim ) AND S.Datum IN ( "+
-        		  			"Select distinct Datum FROM Spiel "+
-        		  			"Order by Datum desc "+
-        		  			"FETCH FIRST 3 ROWS ONLY) "+
-        		  			"Group By V.Name "+
-        		  			"Union all "+
-        		  			"SELECT V.Name, sum(S.Tore_Gast) "+ 
-        		  			"FROM Verein V,Spiel S "+
-        		  			"WHERE (V.V_ID = S.Gast ) AND S.Datum IN ( "+
-        		  			"Select distinct Datum FROM Spiel "+
-        		  			"Order by Datum desc "+ 
-        		  			"FETCH FIRST 3 ROWS ONLY) "+
-        		  			"Group By V.Name "+
-        		  			") neu "+
-        		  			"Group by neu.Name ";
+          String query1 = "Select neu.Name, sum(neu.Tore)"+
+        		  		  " From ("+
+        				  " SELECT V.Name, sum(S.Tore_Heim) as Tore"+
+        				  " FROM Verein V,Spiel S"+
+        				  " WHERE (V.V_ID = S.Heim ) AND S.Spieltag IN ("+
+        				  " Select distinct Spieltag FROM Spiel"+
+        				  " Order by Spieltag desc "+
+        				  " FETCH FIRST 3 ROWS ONLY)"+
+        				  " Group By V.Name"+
+        				  " Union all"+
+        				  " SELECT V.Name, sum(S.Tore_Gast)"+ 
+        				  " FROM Verein V,Spiel S"+
+        				  " WHERE (V.V_ID = S.Gast ) AND S.Spieltag IN ("+
+        				  " Select distinct Spieltag FROM Spiel"+
+        				  " Order by Spieltag desc"+
+        				  " FETCH FIRST 3 ROWS ONLY)"+
+        				  " Group By V.Name"+
+        				  " ) neu"+
+        				  " Group by neu.Name";
+          
           
           
           
           ResultSet resultSet = statement.executeQuery (	query1	  );
           ResultSetMetaData resultSetMetaData = resultSet.getMetaData ();
+      
      
-          System.out.println ("\n Feature 2: Tore der letzten 3 Spiele\n");
+          System.out.println ("\n Feature 1: Tore der letzten 3 Spiele\n");
      
           String format = "%35s   %-6s   \n";
      
@@ -178,6 +145,126 @@
       }
 
       
+    //Feature2: 'Gegentore der letzten 3 Spiele' 
+      private void showGegenTore() {
+        try {
+          Statement statement = connection.createStatement ();
+     
+          String query2 = "Select neu.Name, sum(neu.Tore)"+
+        		  			" From ("+
+        					" SELECT V.Name, sum(S.Tore_Gast) as Tore"+
+        					" FROM Verein V,Spiel S"+
+        					" WHERE (V.V_ID = S.Heim ) AND S.Spieltag IN ("+
+        					" Select distinct Spieltag FROM Spiel"+
+        					" Order by Spieltag desc"+
+        					" FETCH FIRST 3 ROWS ONLY)"+
+        					" Group By V.Name"+
+        					" Union all"+
+        					" SELECT V.Name, sum(S.Tore_Heim)"+ 
+        					" FROM Verein V,Spiel S"+
+        					" WHERE (V.V_ID = S.Gast ) AND S.Spieltag IN ("+
+        					" Select distinct Spieltag FROM Spiel"+
+        					" Order by Spieltag desc"+
+        					" FETCH FIRST 3 ROWS ONLY)"+
+        					" Group By V.Name"+
+        					" ) neu"+
+        					" Group by neu.Name";
+          
+          
+          
+          
+          ResultSet resultSet = statement.executeQuery (	query2	  );
+          ResultSetMetaData resultSetMetaData = resultSet.getMetaData ();
+      
+     
+          System.out.println ("\n Feature 2: Gegentore der letzten 3 Spiele\n");
+     
+          String format = "%35s   %-6s   \n";
+     
+          System.out.printf (format + "\n",
+                             resultSetMetaData.getColumnLabel (1),
+                             resultSetMetaData.getColumnLabel (2)
+                             );
+
+          while (resultSet.next ())
+          {
+            System.out.printf (format,
+                               resultSet.getString (1),
+                               resultSet.getString (2)
+                               );
+          }
+     
+          resultSet.close ();
+          statement.close ();
+        }
+        catch (SQLException e){
+          e.printStackTrace ();
+          System.exit (1);
+        }
+      }
+
+    //Feature3: 'Anzahl der Niederlagen in den letzten 5 Spielen' 
+      private void showNiederlagen() {
+        try {
+          Statement statement = connection.createStatement ();
+     
+          String query3 = "Select neu.Name, sum(neu.Niederlagen)"+
+        		  			" From ("+
+        					" SELECT V.Name, count(*) as Niederlagen"+
+        					" FROM Verein V,Spiel S"+
+        					" WHERE (V.V_ID = S.Heim )"+ 
+        					" AND (S.Tore_Heim < S.Tore_Gast)"+  
+        					" AND S.Spieltag IN ("+
+        					" Select distinct Spieltag FROM Spiel"+
+        					" Order by Spieltag desc"+
+        					" FETCH FIRST 5 ROWS ONLY)"+
+        					" Group By V.Name"+
+        					" Union all"+
+        					" SELECT V.Name, count(*)"+ 
+        					" FROM Verein V,Spiel S"+
+        					" WHERE (V.V_ID = S.Gast )"+ 
+        					" AND (S.Tore_Heim > S.Tore_Gast)"+
+        					" AND S.Spieltag IN ("+
+        					" Select distinct Spieltag FROM Spiel"+
+        					" Order by Spieltag desc"+
+        					" FETCH FIRST 5 ROWS ONLY)"+
+        					" Group By V.Name"+
+        		  			" ) neu"+
+        		  			" Group by neu.Name";
+          
+          
+          
+          
+          ResultSet resultSet = statement.executeQuery (	query3	  );
+          ResultSetMetaData resultSetMetaData = resultSet.getMetaData ();
+      
+     
+          System.out.println ("\n Feature 3: Niederlagen der letzten 5 Spiele\n");
+     
+          String format = "%35s   %-6s   \n";
+     
+          System.out.printf (format + "\n",
+                             resultSetMetaData.getColumnLabel (1),
+                             resultSetMetaData.getColumnLabel (2)
+                             );
+
+          while (resultSet.next ())
+          {
+            System.out.printf (format,
+                               resultSet.getString (1),
+                               resultSet.getString (2)
+                               );
+          }
+     
+          resultSet.close ();
+          statement.close ();
+        }
+        catch (SQLException e){
+          e.printStackTrace ();
+          System.exit (1);
+        }
+      }
+
       
       
       public static void main (String[] args)
